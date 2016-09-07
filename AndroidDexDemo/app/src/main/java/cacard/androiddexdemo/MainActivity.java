590a424107bcba00537709f6ca6872576a5d3485
cacard <cacard@126.com>
2016-09-07 20:38:59 +0800

    complete

--- AndroidDexDemo/app/src/main/java/cacard/androiddexdemo/MainActivity.java
+++ AndroidDexDemo/app/src/main/java/cacard/androiddexdemo/MainActivity.java
@@ -1,7 +1,6 @@
 package cacard.androiddexdemo;
 
 import android.app.Activity;
-import android.app.usage.UsageEvents;
 import android.os.Bundle;
 import android.os.Environment;
 
@@ -24,7 +23,7 @@ public class MainActivity extends Activity {
 
     /**
      * DexClassLoader可以从未安装的.dex/.apk/.jar中加载
-     * <p/>
+     * <p>
      * 1, 普通.class -> .jar
      * 2，通过dx tool .jar -> .dex(.apk/.jar)
      * 3，Load Class From .dex
@@ -33,19 +32,23 @@ public class MainActivity extends Activity {
 
         // 要加载的apk所在目录
         String dexPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "dc.apk";
-        // opt释放位置
-        String optDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "dcopt";
+        // opt释放位置 4.1之后不能放到sd
+        String optDir = getDir("_dc_opt", MODE_PRIVATE).getAbsolutePath();
+        checkDir(optDir);
         // 依赖的lib
         String libSearchPath = null;
         // 父Loader
         ClassLoader parentClassLoader = getClassLoader();
 
-        DexClassLoader dexClassLoader = new DexClassLoader(dexPath, optDir, libSearchPath, parentClassLoader);
-
         try {
+            DexClassLoader dexClassLoader = new DexClassLoader(dexPath, optDir, libSearchPath, parentClassLoader);
             Class<?> clazz = dexClassLoader.loadClass(classFullName);
+            String s = clazz.getSimpleName();
         } catch (Exception e) {
             e.printStackTrace();
+
+            //opt放到sd后出错。
+            //Optimized data directory /storage/emulated/0/dcopt is not owned by the current user. Shared storage cannot protect your application from code injection attacks.
         }
     }
 
@@ -54,5 +57,16 @@ public class MainActivity extends Activity {
      */
     private void loadClassFromInstalledApk() {
 
+    }
+
+    private void checkDir(String p) {
+        File f = new File(p);
+        if (!f.exists()) {
+            try {
+                f.mkdir();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
